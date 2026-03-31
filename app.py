ad164bac6bb7733443e4bed97fa056929b136211
Washington <wl2149895@gmail.com>
2026-03-31 15:23:52 -0300

    forcando atualizacao

--- app.py
+++ app.py
@@ -4,17 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '''
-    <h1>🔥 Bot Online</h1>
-    <form action="/chat">
-        <input name="msg" placeholder="Digite algo">
-        <button type="submit">Enviar</button>
-    </form>
-    '''
+    return "🔥 FUNCIONANDO AGORA"
 
 @app.route('/chat')
 def chat():
     msg = request.args.get('msg')
     return f"Você disse: {msg}"
 
-app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
