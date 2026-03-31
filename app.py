4d239129759f37e4e5f9437cf3bbac6e60e2f9f5
Washington <wl2149895@gmail.com>
2026-03-31 14:32:05 -0300

    recriando app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template_string
+import os
 
 app = Flask(__name__)
 
@@ -85,4 +86,5 @@ def chat():
     return jsonify({"resposta": resposta})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
